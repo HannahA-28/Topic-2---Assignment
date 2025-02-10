b35305beb9e2de4e15c357d84516dc108c0c6fb5
Hannah Anderson <339729345@GoTVDSB.ca>
2025-02-10 14:22:05 -0500

    Done

--- Topic 2 - Assignment/Program.cs
+++ Topic 2 - Assignment/Program.cs
@@ -4,15 +4,23 @@
     {
         static void Main(string[] args)
         {
-            //Hannah Anderson
-
+            Console.WriteLine("Hannah Anderson");
+            Console.WriteLine();
             Console.WriteLine(2025 - 17);
+            Console.WriteLine("The year is 2008");
             Console.WriteLine();
             Console.WriteLine((14 + 15 + 16) / 3);
+            Console.WriteLine("The average is 15");
             Console.WriteLine();
             Console.WriteLine((14.0 + 15.0 + 16.0 + 17.0) / 4.0);
+            Console.WriteLine("The average is 15.5");
             Console.WriteLine();
-            Console.WriteLine()
+            Console.WriteLine(Math.PI * (3.0 * 3.0));
+            Console.WriteLine("The area of the circle is 28.3cm*2");
+            Console.WriteLine();
+            Console.WriteLine("When the numbers are being added without quotations and only brackets, then C# knows to add them instead of sticking the numbers together");
+            Console.ReadLine();
+         
 
 
         }
