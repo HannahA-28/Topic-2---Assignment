b51aead3d10cabb3e7f88a23939ec62042f48698
Hannah Anderson <339729345@GoTVDSB.ca>
2025-02-10 13:37:12 -0500

    Add project files.

---
+++ Topic 2 - Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Topic_2___Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hannah Anderson
+
+            Console.WriteLine(2025 - 17);
+            Console.WriteLine();
+            Console.WriteLine((14 + 15 + 16) / 3);
+            Console.WriteLine();
+            Console.WriteLine((14.0 + 15.0 + 16.0 + 17.0) / 4.0);
+            Console.WriteLine();
+            Console.WriteLine()
+
+
+        }
+    }
+}
